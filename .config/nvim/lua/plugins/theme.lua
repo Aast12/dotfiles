3e84b6794c39849e9a61fad8d331e7ec17c5f25f
Andrés Alam Sánchez Torres <aast@MacBook-Air-de-Andres.local>
2025-10-12 15:44:21 -0600

    Add nvim config

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  { "Shatur/neovim-ayu", opts = {
+    mirage = true,
+    terminal = true,
+    overrides = {},
+  } },
+
+  { "LazyVim/LazyVim", opts = {
+    colorscheme = "ayu-mirage",
+  } },
+}
