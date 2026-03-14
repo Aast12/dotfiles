e324db8b74945ce06f1ef5da8a618387ef428f1e
Alam Sanchez <asancheztorres1905@gmail.com>
2026-03-14 10:03:17 -0600

    Switch from copilot to supermaven

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,5 +1,6 @@
 return {
   "saghen/blink.cmp",
+  build = "cargo +nightly build --release",
   opts = {
     completion = {
       -- Insert completion item on selection, don't select by default
@@ -11,6 +12,21 @@ return {
     },
     keymap = {
       preset = "super-tab",
+      ["<Tab>"] = {
+        function(cmp)
+          if not cmp.is_visible() then
+            local ok, preview = pcall(require, "supermaven-nvim.completion_preview")
+            if ok and preview.has_suggestion() then
+              preview.on_accept_suggestion()
+              return true
+            end
+          end
+        end,
+        "select_next",
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
     },
     sources = {
       per_filetype = {
